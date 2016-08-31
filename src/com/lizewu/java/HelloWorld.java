a363393710c3413ad0bf9bfccf8195ea689546ee
blench <664697385@qq.com>
2016-08-31 18:46:41 +0800

    继续测试

--- src/com/lizewu/java/HelloWorld.java
+++ src/com/lizewu/java/HelloWorld.java
@@ -1,7 +1,29 @@
 package com.lizewu.java;
 
 public class HelloWorld {
-
+	
+	private String username;
+	private String password;
+	
+	public String getUsername()
+	{
+		return username;
+	}
+	
+	public void setUsername(String userName)
+	{
+		this.username=userName;
+	}
+	
+	public String getPassword()
+	{
+		return password;
+	}
+	
+	public void setPassword(String pwd)
+	{
+		this.password=pwd;
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
