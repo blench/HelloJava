28f8ab6ceaefeda5513ff1f3546dea09161ca826
blench <664697385@qq.com>
2016-09-01 15:22:00 +0800

    username password

--- src/com/lizewu/java/HelloWorld.java
+++ src/com/lizewu/java/HelloWorld.java
@@ -24,12 +24,23 @@ public class HelloWorld {
 	{
 		this.password=pwd;
 	}
+	
+	public HelloWorld()
+	{
+		
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
 		System.out.println("Hello Java");
 		System.out.println("Hello World");
 		System.out.println("Hello Java");
+		HelloWorld hello=new HelloWorld();
+		hello.setUsername("admin");
+		hello.setPassword("1234567");
+		System.out.println("用户名是:"+hello.getUsername());
+		System.out.println("密码是:"+hello.getPassword());
 	}
 
 }
